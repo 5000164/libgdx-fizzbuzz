f1673372cb07771fbb68cffb672a20757c20084f
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-02-11 02:04:55 +0900

    アプリの起動からの経過秒数を表示する

--- core/src/main/scala/jp/_5000164/libgdx_fizzbuzz/View.scala
+++ core/src/main/scala/jp/_5000164/libgdx_fizzbuzz/View.scala
@@ -2,13 +2,17 @@ package jp._5000164.libgdx_fizzbuzz
 
 import com.badlogic.gdx.graphics.g2d.{BitmapFont, SpriteBatch}
 import com.badlogic.gdx.graphics.{Color, GL20}
+import com.badlogic.gdx.utils.TimeUtils
 import com.badlogic.gdx.{Game, Gdx}
 
 class View extends Game {
   var batch: SpriteBatch = _
   var font: BitmapFont = _
+  var startMilliSeconds: Long = _
 
   override def create(): Unit = {
+    startMilliSeconds = TimeUtils.millis()
+
     batch = new SpriteBatch
     font = new BitmapFont
     font.setColor(Color.RED)
@@ -24,6 +28,10 @@ class View extends Game {
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
     batch.begin()
+
+    val elapsedSeconds = (TimeUtils.millis() - startMilliSeconds) / 1000
+    font.draw(batch, elapsedSeconds.toString, 780, 470)
+
     batch.end()
   }
 }
